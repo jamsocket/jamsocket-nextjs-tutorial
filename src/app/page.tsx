23ab8854b29f3f6e94beff1469ab2e723f3aba43
Taylor Baldwin <taylorbaldwin@gmail.com>
2023-06-13 17:58:17 +0000

    completed tutorial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 import 'server-only';
 import HomeContainer from '../components/Home'
+import { init } from '@jamsocket/javascript/server'
 
+const WHITEBOARD_NAME = 'whiteboard-demo/default'
+
+const spawnBackend = init({ dev: true })
+
+// In production, you'll want to do the following instead:
+// const spawnBackend = init({
+//   account: '[YOUR ACCOUNT HERE]',
+//   service: 'whiteboard-demo',
+//   // NOTE: we want to keep the Jamsocket token secret, so we can only do this in a server component
+//   token: '[YOUR TOKEN HERE]',
+// })
 
 export default async function Page() {
-  return <HomeContainer />
+  const spawnResult = await spawnBackend({ lock: WHITEBOARD_NAME })
+  return <HomeContainer spawnResult={spawnResult} />
 }
