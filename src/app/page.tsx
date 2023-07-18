782def28242eeb03fc4bde7d1599b467b92074dd
Taylor Baldwin <taylorbaldwin@gmail.com>
2023-07-18 20:48:26 +0000

    add some more logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,28 @@ import 'server-only';
 import HomeContainer from '../components/Home'
 import { init } from '@jamsocket/javascript/server'
 
+const IS_DEV = process.env.NODE_ENV === 'development'
 const ACCOUNT_NAME = 'taylor'
 const SERVICE_NAME = 'whiteboard-demo-deployed'
 const WHITEBOARD_NAME = `${SERVICE_NAME}/default`
-const JAMSOCKET_API_TOKEN = process.env.JAMSOCKET_API_TOKEN
+const JAMSOCKET_API_TOKEN = process.env.JAMSOCKET_API_TOKEN ?? ''
+const API_URL = IS_DEV ? 'http://localhost:8080' : undefined
 
-const IS_DEV = process.env.NODE_ENV === 'development'
-if (!IS_DEV && typeof JAMSOCKET_API_TOKEN !== 'string') {
+if (!IS_DEV && !JAMSOCKET_API_TOKEN) {
   throw new Error(`Expected JAMSOCKET_API_TOKEN in environment`)
 }
+
+console.log('Using the following config:', { IS_DEV, ACCOUNT_NAME, SERVICE_NAME, API_URL })
 
 const spawnBackend = init({
   account: ACCOUNT_NAME,
   service: SERVICE_NAME,
-  token: JAMSOCKET_API_TOKEN ?? '',
-  apiUrl: IS_DEV ? 'http://localhost:8080' : undefined
+  token: JAMSOCKET_API_TOKEN,
+  apiUrl: API_URL
 })
 
 export default async function Page() {
   const spawnResult = await spawnBackend({ lock: WHITEBOARD_NAME })
+  console.log('spawned backend', spawnResult)
   return <HomeContainer spawnResult={spawnResult} />
 }
