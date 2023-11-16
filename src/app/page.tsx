f92d60dcf076c2944e36aaf29053a3c77b00dde5
Felicia <ffeliciachang@gmail.com>
2023-11-15 16:26:37 -0800

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import { init } from '@jamsocket/javascript/server'
 const WHITEBOARD_NAME = 'whiteboard-demo/default'
 
 const spawnBackend = init({
-  account: '[FILL ME IN]',
+  account: 'felicia',
   service: 'whiteboard-demo',
   // NOTE: we want to keep the Jamsocket token secret, so we can only do this in a server component
   // We'll leave this blank for now, since we don't need it when developing with the dev CLI
-  token: '',
+  token: 'hSJfNOIaNyR4YGpq.NzCtO8StvEymuCRoSfor6uWHi9776u',
   apiUrl: 'http://localhost:8080'
 })
 
